ec26d1608e3015a2d170066aca835238a7e15729
chelsangya <csangya@gmail.com>
2023-12-04 08:33:22 +0545

    calc app

---
+++ lib/app/app.dart
@@ -0,0 +1,15 @@
+import 'package:calculator_app/view/calculator_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      home: CalculatorView(),
+    );
+  }
+}
